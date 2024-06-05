88cc32d536e4061dad1d2d3fc2e0e03ba3ede5c8
Hans Sorongon <hanssorongonn@gmail.com>
2024-06-05 15:15:30 +0800

    feat: add functionality to select and deselect columns

--- components/assetTable/AssetTable.tsx
+++ components/assetTable/AssetTable.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dayjs from 'dayjs'
+import { titles } from '../../common/types';
 import { useState, useEffect, Dispatch, SetStateAction } from 'react'
 import { Center, ActionIcon, Button } from '@mantine/core';
 import { DataTable, DataTableColumn } from 'mantine-datatable';
@@ -15,6 +16,7 @@ interface AssetTableProps {
   setSelectedRecords: any, // set to any to silence the bug
   isMutating?: boolean,
   data?: Asset[]
+  columns: string[],
 }
 
 const renderActions: DataTableColumn<Asset>['render'] = (record) => (
@@ -25,7 +27,32 @@ const renderActions: DataTableColumn<Asset>['render'] = (record) => (
   </Center>
 )
 
-export default function AssetTable({ selectedRecords, setSelectedRecords, isMutating, data }: AssetTableProps) {
+function generateColumns(columns: string[]): DataTableColumn<Asset>[] {
+
+  const columnProps = []
+
+  for (let title in titles) {
+    if (columns.includes(title)) {
+      if (title.toLowerCase().includes('date')) {
+        columnProps.push({
+          accessor: title,
+          title: titles[title as keyof typeof titles],
+          render: ({ date }: { date: string }) => dayjs(date).format('MMM DD, YYYY'),
+        })
+      } else {
+        columnProps.push({ accessor: title, title: titles[title as keyof typeof titles] })
+      }
+    }
+  }
+
+  columnProps.push({ accessor: 'actions', title: 'Update', width: '0%', render: renderActions })
+
+  return columnProps as DataTableColumn<Asset>[];
+}
+
+
+
+export default function AssetTable({ selectedRecords, setSelectedRecords, isMutating, data, columns }: AssetTableProps) {
 
   const [page, setPage] = useState(1);
   const [records, setRecords] = useState<Asset[]>([]);
@@ -33,12 +60,10 @@ export default function AssetTable({ selectedRecords, setSelectedRecords, isMuta
   useEffect(() => {
     const from = (page - 1) * PAGE_SIZE;
     const to = from + PAGE_SIZE;
-
     if (data) {
       setRecords(data.slice(from, to));
     }
   }, [data, page]);
-
 
   return (
     <>
@@ -49,39 +74,7 @@ export default function AssetTable({ selectedRecords, setSelectedRecords, isMuta
         striped
         highlightOnHover
         records={records}
-        columns={[
-          {
-            accessor: 'assetNum',
-            title: 'Asset No.',
-            width: 80,
-          },
-          { accessor: 'assetType', title: 'Asset Type' },
-          { accessor: 'assetModel', title: 'Model' },
-          { accessor: 'serialNum', title: 'Serial No.' },
-          { accessor: 'bundleNum', title: 'Bundle No.' },
-          { accessor: 'status', title: 'Status' },
-          {
-            accessor: 'statEffDate',
-            title: 'Status Updated',
-            render: ({ statEffDate }) => dayjs(statEffDate).format('MMM DD, YYYY'),
-          },
-          // employee ID
-          { accessor: 'employeeID', title: 'User' },
-          { accessor: 'location', title: 'Location' },
-          { accessor: 'locRemarks', title: 'Location Remarks' },
-          {
-            accessor: 'recInvDate',
-            title: 'Last Inventory',
-            render: ({ recInvDate }) => dayjs(recInvDate).format('MMM DD, YYYY'),
-          },
-
-          {
-            accessor: 'actions',
-            title: 'Update',
-            width: '0%',
-            render: renderActions,
-          }
-        ]}
+        columns={generateColumns(columns)}
         totalRecords={data ? data.length : 0}
         noRecordsText={"The basket is empty!"}
         recordsPerPage={PAGE_SIZE}
@@ -90,7 +83,10 @@ export default function AssetTable({ selectedRecords, setSelectedRecords, isMuta
         selectedRecords={selectedRecords}
         onSelectedRecordsChange={setSelectedRecords}
         fetching={!data || isMutating ? true : false}
+        pinLastColumn
       />
     </>
   );
 }
+
+
