7e00673716090bb791ad10d0e19b85355d4d9baa
Hans Sorongon <hanssorongonn@gmail.com>
2024-06-13 12:48:41 +0800

    fix: make middleware reroute to login on all pages except login and register

--- middleware.ts
+++ middleware.ts
@@ -3,25 +3,36 @@ import { NextResponse, NextRequest } from "next/server";
 
 import { authenticate } from "./actions/actions";
 
+const PUBLIC_FILE = /\.(.*)$/;
+
 export async function middleware(request: NextRequest) {
 
-  const token = request.cookies.get('Auth')?.value
+  const { pathname } = request.nextUrl;
 
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+  if (
+    pathname.startsWith("/_next") || // exclude Next.js internals
+    pathname.startsWith("/static") || // exclude static files
+    PUBLIC_FILE.test(pathname) // exclude all files in the public folder
+  )
+    return NextResponse.next();
 
-  const isAuthenticated = await authenticate(token)
-
-  console.log(isAuthenticated)
-
-  if (isAuthenticated) {
+  if (pathname == '/login' || pathname == '/register') {
     return NextResponse.next()
   }
 
-  return NextResponse.redirect(new URL('/login', request.url))
+  const token = request.cookies.get('Auth')?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  const isAuthenticated = await authenticate(token);
+
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL('/login', request.url));
 }
 
-export const config = {
-  matcher: '/',
-}
+export const config = {}
